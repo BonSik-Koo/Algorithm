25d88eaaecf48216244ee9cdfa8afa5367f579de
bonsik Koo <96917871+BonSik-Koo@users.noreply.github.com>
2023-02-27 23:33:55 +0900

    Add files via upload

---
+++ SWEA/실전실습문제/항공노선관리/Solution.java
@@ -0,0 +1,101 @@
+package SWExpertAcademyProblem.실전실습문제.항공노선관리;
+
+import java.util.Scanner;
+
+class Solution {
+    private final static int CMD_INIT = 1;
+    private final static int CMD_ADD = 2;
+    private final static int CMD_MIN_TRAVEL_TIME = 3;
+    private final static int CMD_MIN_PRICE = 4;
+
+    private final static UserSolution usersolution = new UserSolution();
+
+    private static boolean run(Scanner sc)
+    {
+        int numQuery;
+        int N, mStartAirport, mEndAirport, mStartTime, mTravelTime, mPrice;
+        int userAns, ans;
+
+        boolean isCorrect = false;
+
+        numQuery = sc.nextInt();
+
+        for (int q = 0; q < numQuery; q++)
+        {
+            int cmd;
+            cmd = sc.nextInt();
+
+            switch (cmd)
+            {
+                case CMD_INIT:
+                    N = sc.nextInt();
+                    usersolution.init(N);
+                    isCorrect = true;
+                    break;
+                case CMD_ADD:
+                    mStartAirport = sc.nextInt();
+                    mEndAirport = sc.nextInt();
+                    mStartTime = sc.nextInt();
+                    mTravelTime = sc.nextInt();
+                    mPrice = sc.nextInt();
+                    usersolution.add(mStartAirport, mEndAirport, mStartTime, mTravelTime, mPrice);
+                    break;
+                case CMD_MIN_TRAVEL_TIME:
+                    mStartAirport = sc.nextInt();
+                    mEndAirport = sc.nextInt();
+                    mStartTime = sc.nextInt();
+                    userAns = usersolution.minTravelTime(mStartAirport, mEndAirport, mStartTime);
+                    ans = sc.nextInt();
+
+//                    System.out.println("===Time===");
+//                    System.out.println("my:"+userAns + " ans:"+ans);
+//                    System.out.println();
+
+                    if (userAns != ans)
+                    {
+                        isCorrect = false;
+                    }
+                    break;
+                case CMD_MIN_PRICE:
+                    mStartAirport = sc.nextInt();
+                    mEndAirport = sc.nextInt();
+                    userAns = usersolution.minPrice(mStartAirport, mEndAirport);
+                    ans = sc.nextInt();
+
+//                    System.out.println("===Price===");
+//                    System.out.println("my:"+userAns + " ans:"+ans);
+//                    System.out.println();
+
+                    if (userAns != ans)
+                    {
+                        isCorrect = false;
+                    }
+                    break;
+                default:
+                    isCorrect = false;
+                    break;
+            }
+        }
+        return isCorrect;
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        int T, MARK;
+
+        System.setIn(new java.io.FileInputStream("src/SWExpertAcademyProblem/실전실습문제/res/input6.txt"));
+
+        Scanner sc = new Scanner(System.in);
+
+        T = sc.nextInt();
+        MARK = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++)
+        {
+            int score = run(sc) ? MARK : 0;
+            System.out.println("#" + tc + " " + score);
+        }
+
+        sc.close();
+    }
+}
